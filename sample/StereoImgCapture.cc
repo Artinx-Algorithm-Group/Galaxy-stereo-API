a16f7a7e2f5130758dc3d339c5cfc52c6a2a2175
NERanger <yonglinjing7@gmail.com>
2020-11-20 23:50:07 +0800

    add image save in sample

--- sample/StereoImgCapture.cc
+++ sample/StereoImgCapture.cc
@@ -1,27 +1,32 @@
 #include <csignal>
 #include <opencv2/opencv.hpp>
+#include <string>
 
 #include "StereoCamera/StereoCamera.hpp"
 
 using std::signal;
 using std::cout;
 using std::endl;
+using std::to_string;
 
 using cv::Mat;
 using cv::hconcat;
 using cv::imshow;
 using cv::waitKey;
 using cv::resize;
+using cv::imwrite;
 
 namespace {
-    const uint32_t kExposureTime = 7500;
-    const uint16_t kFrameRate = 150;
+    const uint32_t kExposureTime = 15000;
+    const uint16_t kFrameRate = 120;
 
     // c-style string serial number for code compatibility
-    char left_cam_serial_num[] = "KE0200080462";
-    char right_cam_serial_num[] = "KE0200080465";
+    char left_cam_serial_num[] = "KE0200080465";
+    char right_cam_serial_num[] = "KE0200080462";
 
     bool stop_flag = false;
+
+    int save_frame_index = 0;
 }
 
 void SigintHandler(int sig) {
@@ -49,9 +54,15 @@ int main() {
 
         hconcat(left_color_img, right_color_img, combined_img);
         // Resize image for better display
-        resize(combined_img, combined_img, cv::Size(), 0.6, 0.6);
+        // resize(combined_img, combined_img, cv::Size(), 0.6, 0.6);
         imshow("Stereo output", combined_img);
-        waitKey(1);
+        
+        if(waitKey(10) == 'c') {
+            imwrite("left_" + to_string(save_frame_index) + ".png", left_color_img);
+            imwrite("right_" + to_string(save_frame_index) + ".png", right_color_img);
+            ++save_frame_index;
+            cout << "Stereo frame saved" << endl;
+        }
     }
 
     stereo.StereoStreamOff();
